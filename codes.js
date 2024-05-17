2486e4a558387d5bebe48050d358924a55db0943
runiorjpp <146399769+runiorjpp@users.noreply.github.com>
2024-05-16 22:20:45 -0300

    Add files via upload

---
+++ codes.js
@@ -0,0 +1,10 @@
+const codes = [
+    "for (int i = 0; i < 10; i++) {",
+    "  System.out.println('Informatica');",
+    "}",
+    "".
+    ,
+    "public static void main(String[] args) {",
+    "     System.out.println('Informática');",
+    "}"
+];
---
+++ informatica.js
@@ -0,0 +1,9 @@
+const codes = [
+    "for (int i = 0; i < 10; i++) {",
+    "     System.out.println('Informática');",
+    "}",
+    "",
+    "public static void main(String[] args) {",
+    "    s"
+
+    
